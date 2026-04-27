b6504430b2686c28d8f6e71e04f708e27f838064
Aurélien <aurelien.santi@gmail.com>
2026-04-27 11:51:31 +0200

    Test 10x

--- src/main/scala/SortingTester.scala
+++ src/main/scala/SortingTester.scala
@@ -1,5 +1,6 @@
 import scala.collection.mutable.ArrayBuffer
 import java.io._
+import javax.sound.sampled.AudioSystem
 
 class SortingTester {
   val algos: Array[Sort] = Array[Sort](SelectionSort, YSort)
@@ -16,36 +17,63 @@ class SortingTester {
   val size: Array[Int] = s.toArray
 
   def run(): Unit = {
+    val musicfile2 = new File("./src/main/scala/windows-xp-startup.wav")
+    val clip2 = AudioSystem.getClip()
+    val audio2 = AudioSystem.getAudioInputStream(musicfile2)
+    clip2.open(audio2)
+    clip2.start()
+
     val pw = new PrintWriter(new FileOutputStream("./TestResults/results.csv"))
     pw.println("Algorithme,ArrayFactory,Taille,Temps[ms]")
-    for (factory <- arrayfactories) {
-      for (i <- size) {
-        val a: Array[Int] = factory.create(i)
-        for (algo <- algos) {
-          val a1: Array[Int] = a.clone()
-          var algoName = algo.getClass.getSimpleName()
-          var factoryName = factory.getClass.getSimpleName()
-          algoName = algoName.substring(0, (algoName.length - 1))
-          factoryName = factoryName.substring(0, (factoryName.length - 1))
 
-          pw.print(algoName + ",")
-          pw.print(factoryName + ",")
-          pw.print(i + ",")
 
-          println(s"Algo : $algoName - Factory : $factoryName - Size : $i")
-          val st = System.currentTimeMillis()
+    //for(reps <- 0 to 0){
+      for (factory <- arrayfactories) {
+        for (i <- size) {
+          val a: Array[Int] = factory.create(i)
+          for (algo <- algos) {
+            for(reps <- 0 to 9){
+              val a1: Array[Int] = a.clone()
+              var algoName = algo.getClass.getSimpleName()
+              var factoryName = factory.getClass.getSimpleName()
+              algoName = algoName.substring(0, (algoName.length - 1))
+              factoryName = factoryName.substring(0, (factoryName.length - 1))
 
-          algo.sort(a1)
+              pw.print(algoName + ",")
+              pw.print(factoryName + ",")
+              pw.print(i + ",")
 
-          val duration = System.currentTimeMillis() - st
+              println(s"Algo : $algoName - Factory : $factoryName - Size : $i")
+              val st = System.currentTimeMillis()
 
-          pw.println(duration)
-          println("Duration : " + duration + " [ms]\n")
+              algo.sort(a1)
 
+              val duration = System.currentTimeMillis() - st
+
+              pw.println(duration)
+              println("Duration : " + duration + " [ms]\n")
+
+            }
+          }
         }
       }
-    }
+    //}
+
+    val musicfile = new File("./src/main/scala/windows-xp-shutdown.wav")
+    val clip = AudioSystem.getClip()
+    val audio = AudioSystem.getAudioInputStream(musicfile)
+    clip.open(audio)
+    clip.start()
     pw.close()
+
+    val time: Long = System.currentTimeMillis() + 7500
+
+    //to not kill the fun
+    while (System.currentTimeMillis() < time) {
+      for (i <- 0 to 9) {
+        Thread.sleep(15)
+      }
+    }
   }
 }
 object Tester extends App {
